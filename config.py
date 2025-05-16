4a069b61a439eec8f3ec061f9a37324f28d9e25b
Kapil Parashar <kapil.parashar@getcrest.ai>
2025-05-16 13:41:19 +0530

    configmpu

--- config.py
+++ config.py
@@ -1,7 +1,12 @@
+import os
+
 CONFIG = {
-    "pg_conn_str": "dbname=master_data_fake user=postgres password=password host=postgresql-hl.postgresql.svc.cluster.local port=5432",
-    "bq_project": "your-bq-project",
-    "bq_dataset": "your_bq_dataset",
+    "pg_conn_str": f"dbname=master_data_fake user={os.getenv('PG_USER', 'postgres')} "
+                   f"password={os.getenv('PG_PASSWORD', '')} "
+                   f"host=postgresql-hl.postgresql.svc.cluster.local port=5432",
+
+    "bq_project": "crest-test004-project",
+    "bq_dataset": "mv_master_fake",
     "api_url": "http://flow-api.peerdb.svc.cluster.local/api/v1/mirror",
     "schemas": [f"customer_schema_{i}" for i in range(1, 51)],
     "num_workers": 6,
